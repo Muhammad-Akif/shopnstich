eaffa6bb404658525e86a4bc0e2e05abcf122e52
muhammad-akif <muhammadakif2917@gmail.com>
2021-12-14 18:37:09 +0500

    updated navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,17 +4,17 @@ import Link from 'next/link'
 function Navbar() {
     return (
         <>
-            <nav class="flex items-center justify-between flex-wrap bg-transparent p-6 w-full">
+            <nav class="flex items-center justify-between flex-wrap bg-transparent px-12 py-8 w-full">
                 <div class="flex items-center flex-shrink-0 mr-6">
-                   <Link href="/"><img class="hover:cursor-pointer" src='https://svgshare.com/i/coH.svg'width="150" height="100" title='logo' /></Link>
+                    <Link href="/"><img class="hover:cursor-pointer" src='https://svgshare.com/i/coH.svg' width="150" height="100" title='logo' /></Link>
                 </div>
                 <div class="block lg:hidden">
                     <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
                         <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
                     </button>
                 </div>
-                <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                    <div class="text-lg lg:flex-grow">
+                <div class="w-full block flex-grow lg:flex lg:items-center lg:justify-end lg:w-auto">
+                    <div class="text-lg mr-4">
                         <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                             Men
                         </a>
@@ -31,9 +31,20 @@ function Navbar() {
                             Contact
                         </a>
                     </div>
-                    <div>
+                    <form class="group relative mr-4">
+                        <svg width="20" height="20" fill="currentColor" class="absolute left-3 top-1/2 -mt-2.5 text-gray-400 pointer-events-none group-focus-within:text-blue-500" aria-hidden="true">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
+                        </svg>
+                        <input class="focus:ring-2 focus:ring-blue-500 focus:outline-none w-full text-sm leading-6 text-gray-900 placeholder-gray-400 rounded-md py-2 pl-10 ring-1 ring-gray-200 shadow-sm" type="text" aria-label="Filter projects" placeholder="Filter projects..." />
+                    </form>
+                    <div class="mr-4 text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                        </svg>
+                    </div>
+                    <div class="lg:self-end">
                         <Link href="/login">
-                        <a class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-green-900 mt-4 lg:mt-0">Login</a>
+                            <a class="inline-block text-lg px-4 py-3 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-green-900 mt-4 lg:mt-0">Login</a>
                         </Link>
                     </div>
                 </div>
