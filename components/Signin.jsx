2ccd1af95c52494a2e7fa42218b12df40a626643
muhammad-akif <muhammadakif2917@gmail.com>
2021-12-20 20:29:28 +0500

    updated singup

--- components/Signin.jsx
+++ components/Signin.jsx
@@ -169,6 +169,7 @@
 
 import React from 'react'
 import Link from 'next/link'
+import {FaGoogle} from 'react-icons/fa'
 
 const Signin = ({ inType }) => {
     return (
@@ -182,7 +183,7 @@ const Signin = ({ inType }) => {
 
                 <div class="w-full h-100 text-gray-100">
 
-                    <Link href="/"><img class="hover:cursor-pointer m-auto" src='https://svgshare.com/i/coH.svg' width="180" height="120" title='logo' /></Link>
+                    <Link href="/"><img class="hover:cursor-pointer -mb-8 m-auto" src='https://svgshare.com/i/coH.svg' width="180" title='logo' /></Link>
                     <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12"> {inType ? 'Create a new account' : 'Log in to your account'}</h1>
 
                     <form class="mt-6" action="#" method="POST">
@@ -193,8 +194,8 @@ const Signin = ({ inType }) => {
                             </div>)
                         }
                         <div>
-                            <label class="block text-gray-100">Email Address</label>
-                            <input type="email" name="" id="" placeholder="Enter Email Address" class="w-full px-4 py-3 text-black rounded-lg bg-gray-100 mt-2 border focus:border-green-950 focus:bg-white focus:outline-none" autofocus autocomplete required />
+                            <label class="block mt-4 text-gray-100">Email Address</label>
+                            <input type="email" name="" id="" placeholder="Enter Email Address" class="w-full px-4 py-3 text-black rounded-lg bg-gray-100 mt-2 border focus:border-green-950 focus:bg-white focus:outline-none" required />
                         </div>
                         {
                             inType && (<div class="mt-4">
@@ -204,8 +205,8 @@ const Signin = ({ inType }) => {
                         }
 
                         <div class="mt-4">
-                            <label class="block text-gray-100">Password</label>
-                            <input type="password" name="" id="" placeholder="Enter Password" minlength="6" class="w-full px-4 py-3 text-black rounded-lg bg-gray-100 mt-2 border focus:border-green-950
+                            <label class="block text-gray-100">Password*</label>
+                            <input type="password" name="" id="" placeholder="Confirm Password" minlength="6" class="w-full px-4 py-3 text-black rounded-lg bg-gray-100 mt-2 border focus:border-green-950
                 focus:bg-white focus:outline-none" required />
                         </div>
                         {
@@ -224,9 +225,10 @@ const Signin = ({ inType }) => {
                             <>
                                 <hr class="my-6 border-gray-300 w-full" />
 
-                                <button type="button" class="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
+                                <button type="button" class="w-full block bg-white hover:bg-green-200 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
                                     <div class="flex items-center justify-center">
                                         {/* <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="w-6 h-6" viewBox="0 0 48 48"><defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z" /></defs><clipPath id="b"><use xlink:href="#a" overflow="visible" /></clipPath><path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" /><path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z" /><path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z" /><path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z" /></svg> */}
+                                        <FaGoogle class="text-green-700"/>
                                         <span class="ml-4">
                                             Log in
                                             with
