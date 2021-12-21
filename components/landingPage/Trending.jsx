e4a2dde40cf6b6255d82f0aa6630216225182113
muhammad-akif <muhammadakif2917@gmail.com>
2021-12-21 10:00:35 +0500

    updated trendgs card

--- components/landingPage/Trending.jsx
+++ components/landingPage/Trending.jsx
@@ -53,21 +53,52 @@ const Trending = () => {
                             //         </p>
                             //     </div>
                             // </div>
-                            <div class="flex justify-center items-center">
-                                <div class="container flex justify-center">
-                                    <div class="max-w-sm">
-                                        <div class="bg-white relative shadow-lg hover:shadow-xl transition duration-500 rounded-lg">
-                                            <img class="rounded-t-lg" src="https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80" alt="" />
-                                            <div class="py-6 px-8 rounded-lg bg-white">
-                                                <h1 class="text-gray-700 font-bold text-2xl mb-3 hover:text-gray-900 hover:cursor-pointer">I'm supper dog for you.</h1>
-                                                <p class="text-gray-700 tracking-wide">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, labore. Ea debitis beatae sequi deleniti.</p>
-                                                <button class="mt-6 py-2 px-4 bg-yellow-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300">Buy Now</button>
-                                            </div>
-                                            <div class="absolute top-2 right-2 py-2 px-4 bg-white rounded-lg">
-                                                <span class="text-md">$150</span>
+                            // <div class="flex justify-center items-center">
+                            //     <div class="container flex justify-center">
+                            //         <div class="max-w-sm">
+                            //             <div class="bg-white relative shadow-lg hover:shadow-xl transition duration-500 rounded-lg">
+                            //                 <img class="rounded-t-lg" src="https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80" alt="" />
+                            //                 <div class="py-6 px-8 rounded-lg bg-white">
+                            //                     <h1 class="text-gray-700 font-bold text-2xl mb-3 hover:text-gray-900 hover:cursor-pointer">I'm supper dog for you.</h1>
+                            //                     <p class="text-gray-700 tracking-wide">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, labore. Ea debitis beatae sequi deleniti.</p>
+                            //                     <button class="mt-6 py-2 px-4 bg-yellow-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300">Buy Now</button>
+                            //                 </div>
+                            //                 <div class="absolute top-2 right-2 py-2 px-4 bg-white rounded-lg">
+                            //                     <span class="text-md">$150</span>
+                            //                 </div>
+                            //             </div>
+                            //         </div>
+                            //     </div>
+                            // </div>
+                            <div class="wrapper bg-gray-400 antialiased text-gray-900">
+                                <div>
+
+                                    <img src="https://source.unsplash.com/random/350x350" alt=" random imgee" class="w-full object-cover object-center rounded-lg shadow-md"/>
+
+                                        <div class="relative px-4 -mt-16  ">
+                                            <div class="bg-white p-6 rounded-lg shadow-lg">
+                                                <div class="flex items-baseline">
+                                                    <span class="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
+                                                        New
+                                                    </span>
+                                                    <div class="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
+                                                        2 baths  &bull; 3 rooms
+                                                    </div>
+                                                </div>
+
+                                                <h4 class="mt-1 text-xl font-semibold uppercase leading-tight truncate">A random Title</h4>
+
+                                                <div class="mt-1">
+                                                    $1800
+                                                    <span class="text-gray-600 text-sm">   /wk</span>
+                                                </div>
+                                                <div class="mt-4">
+                                                    <span class="text-teal-600 text-md font-semibold">4/5 ratings </span>
+                                                    <span class="text-sm text-gray-600">(based on 234 ratings)</span>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
+
                                 </div>
                             </div>
                         ))}
