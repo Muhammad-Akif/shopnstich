45ce572ee8507e1821b94d995890df5243537bfd
muhammad-akif <muhammadakif2917@gmail.com>
2022-04-16 01:20:22 +0500

    updated env and animations

--- components/landingPage/MainTextnImage.jsx
+++ components/landingPage/MainTextnImage.jsx
@@ -5,8 +5,8 @@ import tailorImg from '../../images/landingPage/08.png'
 
 const mainTextnImage = () => {
     return (
-        <div class="px-5 sm:mx-auto py-6 sm:py-8 overflow-x-hidden flex flex-col-reverse md:flex-row sm:justify-around sm:items-center">
-            <div data-aos="fade-right" class="text-white py-4 overflow-x-hidden text-xl sm:w-11/12 md:w-1/2 sm:text-4xl xl:mr-20 md:text-5xl font-serif"> <strong class="text-green-950"> Best Place</strong>, Where You Shop And Stitch Your Worth Cloths Tustedly...
+        <div class="px-5 sm:mx-auto py-6 sm:py-8 flex flex-col-reverse md:flex-row sm:justify-around sm:items-center">
+            <div data-aos="fade-up" class="text-white py-4  text-xl sm:w-11/12 md:w-1/2 sm:text-4xl xl:mr-20 md:text-5xl font-serif"> <strong class="text-green-950"> Best Place</strong>, Where You Shop And Stitch Your Worth Cloths Tustedly...
                 <div class="text-xl sm:text-3xl  pt-4 text-orange-300 italic">Your Stitched Order, delivered with in 2 days, over all across Pakistan.   </div>
                 <div class="h-1 my-6 bg-green-950
                  sm:w-11/12 w-full" />
@@ -22,7 +22,7 @@ const mainTextnImage = () => {
                     </div>
                 </div>
             </div>
-            <div data-aos="fade-left" class=" pb-6 sm:pb-0 overflow-x-hidden max-w-2xl self-center"><Image src={tailorImg} width={400} height={400}  alt="tailorImg" /></div>
+            <div data-aos="fade-down" class=" pb-6 sm:pb-0 max-w-2xl self-center"><Image src={tailorImg} width={400} height={400}  alt="tailorImg" /></div>
         </div>
     )
 }
