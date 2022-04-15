8fd6fe44db70197e781bf26e3cd10edb1d8dddb6
muhammad-akif <muhammadakif2917@gmail.com>
2022-04-16 00:27:26 +0500

    remove warnings

--- components/landingPage/MegaMenu.jsx
+++ components/landingPage/MegaMenu.jsx
@@ -17,10 +17,10 @@ const MegaMenu = ({ menu }) => {
                 {
                   menu?.collections.map((collection, key) => (
                     <>
-                      <p class="block px-6 py-2 border-b border-gray-200 w-full uppercase font-semibold text-gray-700">{collection.name}</p>
+                      <p key={key} class="block px-6 py-2 border-b border-gray-200 w-full uppercase font-semibold text-gray-700">{collection.name}</p>
                       {
                         collection.varieties?.map((variety, key) => (
-                          <a href="#!" aria-current="true" class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out flex items-center">
+                          <a href="#!" aria-current="true" key={key} class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out flex items-center">
                             <svg aria-hidden="true" focusable="false" data-prefix="fas" class="w-1.5 mr-1.5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512">
                               <path fill="currentColor" d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"></path>
                             </svg>
