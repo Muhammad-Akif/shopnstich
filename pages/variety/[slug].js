2a0254d839a42bd136b5734a436b6fb6156caf0e
muhammad-akif <muhammadakif2917@gmail.com>
2022-04-19 15:50:13 +0500

    added related products page

--- pages/variety/[slug].js
+++ pages/variety/[slug].js
@@ -1,26 +1,33 @@
 import React from 'react'
-import { getVarieties,getVarietyDetails } from '../../services'
+import { Footer, Navbar } from '../../components'
+import { getVarieties, getVarietyDetails } from '../../services'
 
-const varietyDetails= ({variety}) => {
-  console.log("variety -->",variety)
+const varietyDetails = ({ variety }) => {
+  console.log("variety -->", variety)
   return (
-    <div>varietyDetails</div>
+    <>
+      <Navbar />
+      <RelatedProducts/>
+      <div class="absolute bottom-0">
+        <Footer />
+      </div>
+    </>
   )
 }
 
 export default varietyDetails;
 
 export async function getStaticPaths() { // all Variety slugs to generate paths 
-    const varieties = await getVarieties();
-    return {
-        paths: varieties?.map(({ node: { slug }}) => ({ params: { slug}})),
-        fallback: false
-    }
+  const varieties = await getVarieties();
+  return {
+    paths: varieties?.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: false
+  }
 }
 
 export async function getStaticProps({ params }) {
-    const data = await getVarietyDetails(params?.slug);
-    return {
-      props: { variety: data }
-    }
+  const data = await getVarietyDetails(params?.slug);
+  return {
+    props: { variety: data }
+  }
 }
