358a5346d30b9fc8b75c4ce20b3430e33edda839
muhammad-akif <muhammadakif2917@gmail.com>
2022-04-20 20:27:00 +0500

    UI BreadCrumbs

--- components/landingPage/BreadCrumbs.jsx
+++ components/landingPage/BreadCrumbs.jsx
@@ -1,9 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 const BreadCrumbs = () => {
-  return (
-    <div>BreadCrumbs</div>
-  )
+    return (
+        <nav class="bg-grey-light rounded-md w-full">
+            <ol class="list-reset flex">
+                <li><Link href="#" class="text-green-950 hover:text-green-960">Home</Link></li>
+                <li><span class="text-gray-500 mx-2">{'>'}</span></li>
+                <li><Link href="#" class="text-green-950 hover:text-green-960">Library</Link></li>
+                <li><span class="text-gray-500 mx-2">{'>'}</span></li>
+                <li class="text-gray-500">Data</li>
+            </ol>
+        </nav>
+    )
 }
 
 export default BreadCrumbs
