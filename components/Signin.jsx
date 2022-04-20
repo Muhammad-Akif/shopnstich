416c753c9492338a3e8e3183a5b3749b8b0c52b6
muhammad-akif <muhammadakif2917@gmail.com>
2022-04-20 22:14:24 +0500

    issue resolving in google login

--- components/Signin.jsx
+++ components/Signin.jsx
@@ -16,15 +16,15 @@ const Signin = ({ inType }) => {
         const result = res?.profileObj;
         const token = res?.tokenId;
         try {
-            dispatch({type: "AUTH", data: {result, token}})
+            dispatch({ type: "AUTH", data: { result, token } })
             history.push("/");
         } catch (error) {
             console.log(error)
         }
-    } 
+    }
     const googleFailure = () => {
         console.log("Google Login Failure...")
-    } 
+    }
     const router = useRouter()
     // const dispatch = useDispatch();
     // const classes = useStyles();
@@ -216,6 +216,33 @@ flex items-center justify-center">
                         !inType && (
                             <>
                                 <hr class="my-6 border-gray-300 w-full" />
+                                <GoogleLogin
+                                    clientId="851583521817-5668993p0cmqt1na18v6a5gahnvt5p9j.apps.googleusercontent.com"
+                                    render={(renderProps) => (
+                                        // <Button onClick={renderProps.onClick} disabled={renderProps.disabled}>
+                                        //     Google Sign In
+                                        //     <div class="flex items-center justify-center">
+                                        //         {/* <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="w-6 h-6" viewBox="0 0 48 48"><defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z" /></defs><clipPath id="b"><use xlink:href="#a" overflow="visible" /></clipPath><path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" /><path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z" /><path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z" /><path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z" /></svg> */}
+                                        //         <FaGoogle class="text-green-700" />
+                                        //         <span class="ml-4">
+                                        //         Google Sign In</span>
+                                        //     </div>
+                                        // </Button>
+                                         <button type="button" class="w-full block bg-white hover:bg-green-200 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
+                                         <div class="flex items-center justify-center">
+                                             {/* <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="w-6 h-6" viewBox="0 0 48 48"><defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z" /></defs><clipPath id="b"><use xlink:href="#a" overflow="visible" /></clipPath><path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" /><path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z" /><path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z" /><path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z" /></svg> */}
+                                             <FaGoogle class="text-green-700" />
+                                             <span class="ml-4">
+                                                 Log in
+                                                 with
+                                                 Google</span>
+                                         </div>
+                                     </button>
+                                    )}
+                                    onSuccess={googleSuccess}
+                                    onFailure={googleFailure}
+                                    cookiePolicy="single_host_origin"
+                                />
 
                                 <button type="button" class="w-full block bg-white hover:bg-green-200 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
                                     <div class="flex items-center justify-center">
