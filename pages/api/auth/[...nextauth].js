6cc07b52147994d517d7170eea7e172700ada5f0
muhammad-akif <muhammadakif2917@gmail.com>
2022-05-08 13:35:08 +0500

    updated auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,33 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { compare, hash } from "bcrypt";
 import { GraphQLClient } from "graphql-request";
 
+export async function asynchandler(req, res) {
+    const client = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT, {
+        headers: {
+            Authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+        },
+    });
+
+    const GetUserByEmail = gql`
+    query GetUserByEmail($email: String!) {
+      user: nextUser(where: { email: $email }, stage: DRAFT) {
+        id
+        password
+      }
+    }
+  `;
+
+    const CreateNextUserByEmail = gql`
+    mutation CreateNextUserByEmail($email: String!, $password: String!) {
+      newUser: createNextUser(data: { email: $email, password: $password }) {
+        id
+      }
+    }
+  `;
+}
+
 export default NextAuth({
+    
     providers: [
         CredentialsProvider({
             name: "Email and Password",
@@ -19,64 +45,39 @@ export default NextAuth({
                     placeholder: "Password"
                 },
             },
+            authorize: async ({ email, password }) => {
+                const { user } = await client.request(GetUserByEmail, {
+                    email,
+                });
+
+                if (!user) {
+                    const { newUser } = await client.request(
+                        CreateNextUserByEmail,
+                        {
+                            email,
+                            password: await hash(password, 12),
+                        }
+                    );
+
+                    return {
+                        id: newUser.id,
+                        username: email,
+                        email,
+                    };
+                }
+
+                const isValid = await compare(password, user.password);
+
+                if (!isValid) {
+                    throw new Error("Wrong credentials. Try again.");
+                }
+
+                return {
+                    id: user.id,
+                    username: email,
+                    email,
+            };
         },
+    }),
     ]
-})
-
-const client = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT, {
-    headers: {
-        Authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
-    },
 });
-
-const GetUserByEmail = gql`
-  query GetUserByEmail($email: String!) {
-    user: nextUser(where: { email: $email }, stage: DRAFT) {
-      id
-      password
-    }
-  }
-`;
-
-const CreateNextUserByEmail = gql`
-  mutation CreateNextUserByEmail($email: String!, $password: String!) {
-    newUser: createNextUser(data: { email: $email, password: $password }) {
-      id
-    }
-  }
-`;
-
-authorize: async ({ email, password }) => {
-    const { user } = await client.request(GetUserByEmail, {
-      email,
-    });
-    
-    if (!user) {
-      const { newUser } = await client.request(
-        CreateNextUserByEmail,
-        {
-          email,
-          password: await hash(password, 12),
-        }
-      );
-  
-      return {
-        id: newUser.id,
-        username: email,
-        email,
-      };
-    }
-  
-    const isValid = await compare(password, user.password);
-  
-    if (!isValid) {
-      throw new Error("Wrong credentials. Try again.");
-    }
-  
-    return {
-      id: user.id,
-      username: email,
-      email,
-    };
-  },
-  
