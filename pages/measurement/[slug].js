57a730e1712ff66d3eb9e900973d61eea81e50ef
muhammad-akif <muhammadakif2917@gmail.com>
2022-05-20 11:09:28 +0500

    added men measurement details

--- pages/measurement/[slug].js
+++ pages/measurement/[slug].js
@@ -1,6 +1,22 @@
 import React from 'react'
 
 const measurement = () => {
+    const menDetails = [
+        {image: 'https://www.crowntailor.com/images/menm/men_size_1.jpg', title: "FULL SHOULDER WIDTH", description: "Measure from the end of the right shoulder to the end of the left shoulder. We define the 'End of the Shoulder as where the horizontal plane of the shoulder intersects thevertical plane of the arm'"},
+        {image: 'https://www.crowntailor.com/images/menm/men_size_2.jpg', title: "SLEEVES", description: "The 'Pinch' of the hand is found where the base of the thumb and the base of the index finger intersect"},
+        {image: 'https://www.crowntailor.com/images/menm/men_size_3.jpg', title: "FULL CHEST", description: "Measure around the chest and body at its fullest part (typically right under the armpits and shoulder blades, right across the nipples). Hold the tape against the body tight enough so that it does not slip down, but not so tight that it restricts breathing. It should be 'snug'. Make sure the tape is horizontal and theperson's arms are by their side; then take a normal breath and measure. Do not puff out your chest."},
+        {image: 'https://www.crowntailor.com/images/menm/men_size_4.jpg', title: "WAIST", description: "Measure around the fullest part of the waist/stomach or right below your bottom ribs, whichever is greater."},
+        {image: 'https://www.crowntailor.com/images/menm/men_size_5.jpg', title: "HIPS/ SEAT", description: "Measure around the fullest part of your hips and buttocks."},
+        {image: 'https://www.crowntailor.com/images/menm/men_size_6.jpg', title: "FRONT SHOULDER WIDTH", description: "With the person's arms by their side, measure across the front of the chest. Start at the front of the right armpit, continue measuring across the nipples, and then stop at the front of the left armpit. Do not measure under the arms."},
+        {image: 'https://www.crowntailor.com/images/menm/men_size_7.jpg', title: "BACK SHOULDER WIDTH", description: "With the person's arms by their side, measure across the back. Start at the back of the right armpit, continue measuring across the shoulder blades, and then stop at the back of the left armpit. Do not measure under the arms."},
+        {image: 'https://www.crowntailor.com/images/menm/men_size_8.jpg', title: "FRONT JACKET LENGTH", description: "Measure straight down the front from the base of the neck (right or left side) to the point level with your thumb knuckle.or till the length you would like your jacket"},
+        {image: 'https://www.crowntailor.com/images/menm/men_size_9.jpg', title: "NECK", description: "The tape should be comfortably touching the skin, with enough room so that you do not feel restricted."},
+        {image: 'https://www.crowntailor.com/images/menm/men_size_10.jpg', title: "TROUSER WAIST", description: "Measure around your waist at the level where you would normally wear your pant's belt. With the tape measure snug around your waist, relax, and take the measurement. Don't be alarmed if this measuremen seems larger than expected; off-the-rack pants are normally labeled as being smaller than what they really are."},
+        {image: 'https://www.crowntailor.com/images/menm/men_size_11.jpg', title: "CROTCH", description: "Measure from the front top of the pant's waistband to the back top of the pant's waistband."},
+        {image: 'https://www.crowntailor.com/images/menm/men_size_12.jpg', title: "THIGH", description: "Measure around your thigh at its widest point. You need measure only one side."},
+        {image: 'https://www.crowntailor.com/images/menm/men_size_13.jpg', title: "TROUSER'S LENGTH", description: "Measure from the top of your pant’s waistband to the floor along the outside of your leg. Make sure the tape is tight, that you are standing straight, and then measure."},
+        {image: 'https://www.crowntailor.com/images/menm/men_size_14.jpg', title: "CUFF", description: "Measure width around cuff as shown."},
+    ]
   return (
     <div>measurement</div>
   )
