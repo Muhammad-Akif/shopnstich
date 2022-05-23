7daba814879cbf7c088f7b1d0fefaa46e43f0aaa
muhammad-akif <muhammadakif2917@gmail.com>
2022-05-23 15:50:45 +0500

    done with basic image uploadation

--- pages/upload.js
+++ pages/upload.js
@@ -2,35 +2,41 @@ import React, { useState } from 'react';
 import ProgressBar from '../components/ProgressBar';
 
 const UploadForm = () => {
-  const [file, setFile] = useState(null);
-  const [error, setError] = useState(null);
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
 
-  const types = ['image/png', 'image/jpeg'];
+    const types = ['image/png', 'image/jpeg'];
 
-  const handleChange = (e) => {
-    let selected = e.target.files[0];
+    const handleChange = (e) => {
+        let selected = e.target.files[0];
 
-    if (selected && types.includes(selected.type)) {
-      setFile(selected);
-      setError('');
-    } else {
-      setFile(null);
-      setError('Please select an image file (png or jpg)');
-    }
-  };
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+            setError('');
+        } else {
+            setFile(null);
+            setError('Please select an image file (png or jpg)');
+        }
+    };
 
-  return (
-    <form>
-      <label>
-        <input type="file" onChange={handleChange} />
-      </label>
-      <div className="output">
-        { error && <div className="error">{ error }</div>}
-        { file && <div>{ file.name }</div> }
-        { file && <ProgressBar file={file} setFile={setFile} /> }
-      </div>
-    </form>
-  );
+    return (
+        <form>
+            <div class="flex w-full h-screen items-center justify-center bg-grey-lighter">
+                <label class="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white">
+                    <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+                    </svg>
+                    <span class="mt-2 text-base leading-normal">Upload Image</span>
+                    <input type='file' onChange={handleChange} class="hidden" />
+                </label>
+            </div>
+            <div className="output">
+                {error && <div className="error">{error}</div>}
+                {file && <div>{file.name}</div>}
+                {file && <ProgressBar file={file} setFile={setFile} />}
+            </div>
+        </form>
+    );
 }
 
 export default UploadForm;
