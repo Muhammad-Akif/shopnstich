989fe4c4064ad77c824236f3f24b44a8378dcf34
muhammad-akif <muhammadakif2917@gmail.com>
2022-05-26 23:08:29 +0500

    updated completed tailor info

--- pages/tailorInfo.js
+++ pages/tailorInfo.js
@@ -4,8 +4,8 @@ import { useStateContext } from '../context/StateContext';
 
 const tailorInfo = () => {
     const [error, setError] = useState(null);
-    const [isGender, setGender] = useState(true)
-    const [isState, setState] = useState(true)
+    const [isGender, setGender] = useState(false)
+    const [isState, setState] = useState(false)
     const { file, setFile, tailorInfo, setTailorInfo } = useStateContext();
 
     const types = ['image/png', 'image/jpeg'];
@@ -34,20 +34,21 @@ const tailorInfo = () => {
 
     const formSubmit = (e) => {
         e.preventDefault();
-        if (tailorInfo.gender.length > 1 ) {
+        if (tailorInfo.gender.length < 1 ) {
             setGender(true)
         }
-        else if(tailorInfo.province.length > 1) {
+        else if(tailorInfo.province.length < 1) {
             setState(true)
         }
         else {
             setGender(false)
             setState(false)
+            console.log("obj --> ",tailorInfo)
         }
     }
 
     return (
-        <div class="p-5 bg-gray-50">
+        <div class="p-5 sm:px-8 bg-gray-50">
             <div class="mx-4 p-4 pb-24">
                 <div class="flex items-center justify-center">
 
@@ -84,13 +85,13 @@ const tailorInfo = () => {
                     <div class="w-full mx-2 flex-1">
                         <div class="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> Full Name</div>
                         <div class="bg-white my-2 p-1 flex border border-gray-200 rounded">
-                            <input required name="fullname" onChange={InputEvent} placeholder="Enter Your Name" class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
+                            <input required name="fullname" value={tailorInfo.fullname} onChange={InputEvent} placeholder="Enter Your Name" class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
                         </div>
                     </div>
                     <div class="w-full mx-2 flex-1">
                         <div class="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> Email</div>
                         <div class="bg-white my-2 p-1 flex border border-gray-200 rounded">
-                            <input required name="email" onChange={InputEvent} placeholder="Email Address" type='email' class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
+                            <input required name="email" value={tailorInfo.email} onChange={InputEvent} placeholder="Email Address" type='email' class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
                         </div>
                     </div>
                 </div>
@@ -98,28 +99,28 @@ const tailorInfo = () => {
                     <div class="w-full mx-2 flex-1">
                         <div class="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> Phone</div>
                         <div class="bg-white my-2 p-1 flex border border-gray-200 rounded">
-                            <input required name="phone" onChange={InputEvent} placeholder="03*********" type="number" class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
+                            <input required name="phone" value={tailorInfo.phone} onChange={InputEvent} placeholder="03*********" type="number" class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
                         </div>
                     </div>
                     <div class="w-full mx-2 flex-1">
                         <div class="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> Age</div>
                         <div class="bg-white my-2 p-1 flex border border-gray-200 rounded">
-                            <input required name="age" onChange={InputEvent} placeholder="22" type="number" class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
+                            <input required name="age" value={tailorInfo.age} onChange={InputEvent} placeholder="22" type="number" class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
                         </div>
                     </div>
                 </div>
                 <div class="py-5 mx-3">
                     <label for="exampleFormControlTextarea1" class="form-label font-bold text-gray-600 text-xs leading-8 uppercase inline-block mb-2"
                     >Bio : </label>
-                    <textarea required onChange={InputEvent} placeholder="Enter Bio Details..." name="other" id="exampleFormControlTextarea1" class="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none" rows="4"></textarea>
+                    <textarea required name="bio" value={tailorInfo.bio} onChange={InputEvent} placeholder="Enter Bio Details..." id="exampleFormControlTextarea1" class="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none" rows="4"></textarea>
                 </div>
                 <div class="flex flex-col md:flex-row">
                     <div class="w-full mx-2 flex-1 relative">
                         <div class="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> Gender</div>
-                        <select required name="gender" onChange={InputEvent} class="block mt-2 appearance-none w-full bg-gray-200 border border-gray-200 py-2.5 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <select required name="gender" value={tailorInfo.gender} onChange={InputEvent} class="block mt-2 appearance-none w-full bg-gray-200 border border-gray-200 py-2.5 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
                             <option class="text-gray-300" >select</option>
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
                         </select>
                         <div class="pointer-events-none absolute inset-y-0 right-0  top-10 flex items-center px-2 text-gray-700">
                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
@@ -128,41 +129,36 @@ const tailorInfo = () => {
 
                     <div class="w-full mx-2 flex-1 relative">
                         <div class="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> State / Province</div>
-                        <select required name="gender" onChange={InputEvent} class="block mt-2 appearance-none w-full bg-gray-200 border border-gray-200 py-2.5 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <select required name="province" value={tailorInfo.province} onChange={InputEvent} class="block mt-2 appearance-none w-full bg-gray-200 border border-gray-200 py-2.5 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
                             <option class="text-gray-300" >select</option>
-                            <option value="male">Punjab</option>
-                            <option value="male">Sindh</option>
-                            <option value="male">Khyber Pakhtunkhwa</option>
-                            <option value="male">Balochistan</option>
+                            <option value="Punjab">Punjab</option>
+                            <option value="Sindh">Sindh</option>
+                            <option value="Khyber Pakhtunkhwa">Khyber Pakhtunkhwa</option>
+                            <option value="Balochistan">Balochistan</option>
                         </select>
                         <div class="pointer-events-none absolute inset-y-0 right-0  top-10 flex items-center px-2 text-gray-700">
                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
                         </div>
                     </div>
-                    {!isState && (
-                        <div class="text-sm text-red-500 p-2 px-2">Gender Required !</div>
-                    )}
                 </div>
-                {!isGender && (
+                {isGender && (
                     <div class="text-sm text-red-500 p-2 px-2">Gender Required !</div>
                 )}
-                {!isState && (
+                {isState && (
                     <div class="text-sm text-red-500 p-2 px-2">State / Province Required !</div>
                 )}
-                {/* {!isGender && (
-                    <div class="text-sm text-red-500 p-2 px-2">Gender Required !</div>
-                )} */}
+              
                 <div class="flex flex-col md:flex-row">
                     <div class="w-full mx-2 flex-1">
                         <div class="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> City</div>
                         <div class="bg-white my-2 p-1 flex border border-gray-200 rounded">
-                            <input required name="phone" onChange={InputEvent} placeholder="Enter City..." class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
+                            <input required name="city" value={tailorInfo.city} onChange={InputEvent} placeholder="Enter City..." class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
                         </div>
                     </div>
                     <div class="w-full mx-2 flex-1">
                         <div class="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> ZIP / Postal code</div>
                         <div class="bg-white my-2 p-1 flex border border-gray-200 rounded">
-                            <input required name="age" onChange={InputEvent} placeholder="Enter Code..." type="number" class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
+                            <input required name="zip" value={tailorInfo.zip} onChange={InputEvent} placeholder="Enter Code..." type="number" class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
                         </div>
                     </div>
                 </div>
@@ -170,7 +166,7 @@ const tailorInfo = () => {
                     <div class="w-full mx-2 flex-1">
                         <div class="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> Street address</div>
                         <div class="bg-white my-2 p-1 flex border border-gray-200 rounded">
-                            <input required name="phone" onChange={InputEvent} placeholder="Enter Address..." class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
+                            <input required name="address" value={tailorInfo.address} onChange={InputEvent} placeholder="Enter Address..." class="p-1 px-2 appearance-none outline-none w-full text-gray-800" />
                         </div>
                     </div>
                 </div>
