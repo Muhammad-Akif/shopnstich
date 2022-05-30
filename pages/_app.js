cef6d5dce78c7194c910b3d05a8e656685aec614
muhammad-akif <muhammadakif2917@gmail.com>
2022-05-30 18:53:02 +0500

    updated google login

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import AOS from "aos";
 import { Toaster } from 'react-hot-toast';
+import Head from 'next/head'
 import { StateContext } from '../context/StateContext';
 import "aos/dist/aos.css";
 import '../styles/globals.css'
@@ -14,10 +15,26 @@ function MyApp({ Component, pageProps }) {
     });
   }, []);
   return (
-    <StateContext>
-      <Toaster />
-      <Component {...pageProps} />
-    </StateContext>
+    <>
+      <Head>
+        <title>ShopnStitch</title>
+        <link rel="icon" href="/favicon.ico" />
+        <script src="https://accounts.google.com/gsi/client" async defer></script>
+        <div id="g_id_onload"
+          data-client_id="851583521817-5668993p0cmqt1na18v6a5gahnvt5p9j.apps.googleusercontent.com"
+          data-callback="handleCredentialResponse">
+        </div>
+        <div class="g_id_signin" data-type="standard"></div>
+        <script src="./TW-ELEMENTS-PATH/dist/js/index.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@3.6.2/dist/chart.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+      </Head>
+      <StateContext>
+        <Toaster />
+        <Component {...pageProps} />
+      </StateContext>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import Head from 'next/head'
 import { getAllProducts } from '../services'
 import { Navbar } from '../components'
 import { MainTextnImage, MapnConnect, Trending, ProductCard, Reviews, Footer } from '../components'
@@ -7,20 +5,6 @@ import { MainTextnImage, MapnConnect, Trending, ProductCard, Reviews, Footer } f
 export default function Home({ products }) {
   return (
     <>
-      <Head>
-        <title>ShopnStitch</title>
-        <link rel="icon" href="/favicon.ico" />
-        <script src="https://accounts.google.com/gsi/client" async defer></script>
-        <div id="g_id_onload"
-          data-client_id="851583521817-5668993p0cmqt1na18v6a5gahnvt5p9j.apps.googleusercontent.com"
-          data-callback="handleCredentialResponse">
-        </div>
-        <div class="g_id_signin" data-type="standard"></div>
-        <script src="./TW-ELEMENTS-PATH/dist/js/index.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/chart.js@3.6.2/dist/chart.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-      </Head>
 
       <div class="bg-green-960 sm:min-h-screen">
         <Navbar products={products} />
